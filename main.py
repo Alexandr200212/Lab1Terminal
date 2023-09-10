7e12673b23ca541ce0a141cea5c9839364fb53ff
Alexandr200212 <123861537+Alexandr200212@users.noreply.github.com>
2023-09-10 14:48:18 +0700

    shmykovPIE-01(2)

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 def greeting():
 	print("Привет, пользователь!")
+
+def get_name():
+	name = input("Как тебя зовут")
+	return name
+
+def get_age():
+	age = int(input("Сколько тебе лет"))
+	return age
+
+greeting()
+name = get_name()
+age = get_age()
+
+print("Тебя зовут "+name+" и тебе "+str(age)+" лет")
